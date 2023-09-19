ea8ec994664c95949f11fe856a13ae6dac96e44f
dongyoungs <kkssckkssc#gmail.com>
2023-09-19 10:48:49 +0900

    fix : Enum 형태의 값도 DB 데이터베이스에 저장되도록 수정  (homeController 쪽에 유저 DB에 추가하는 예시 주석으로 남겨놨으니 참고하세요)

--- userData.sql
+++ userData.sql
@@ -1,10 +1,10 @@
 insert into auction_user(authority,email,password,name,phone,zonecode,address,detail_addr)
-values(1,'aaa@test.com',sha1('1111'), 'aaa','010-1234-5678','12345','주소','상세주소');
+values("USER",'aaa@test.com',sha1('1111'), 'aaa','010-1234-5678','12345','주소','상세주소');
 insert into auction_user(authority,email,password,name,phone,zonecode,address,detail_addr)
-values(1,'aaa@test.com',sha1('1111'), 'bbb','010-3333-4444','12345','주소1','상세주소1');
+values("USER",'aaa@test.com',sha1('1111'), 'bbb','010-3333-4444','12345','주소1','상세주소1');
 insert into auction_user(authority,email,password,name,phone,zonecode,address,detail_addr)
-values(1,'aaa@test.com',sha1('1111'), 'ccc','010-1577-1577','12345','주소2','상세주소2');
+values("USER",'aaa@test.com',sha1('1111'), 'ccc','010-1577-1577','12345','주소2','상세주소2');
 insert into auction_user(authority,email,password,name,phone,zonecode,address,detail_addr)
-values(1,'aaa@test.com',sha1('1111'), 'ddd','010-8282-8282','12345','주소3','상세주소3');
+values("USER",'aaa@test.com',sha1('1111'), 'ddd','010-8282-8282','12345','주소3','상세주소3');
 insert into auction_user(authority,email,password,name,phone,zonecode,address,detail_addr)
-values(1,'aaa@test.com',sha1('1111'), 'eee','010-5544-4455','12345','주소4','상세주소4');
+values("USER",'aaa@test.com',sha1('1111'), 'eee','010-5544-4455','12345','주소4','상세주소4');
