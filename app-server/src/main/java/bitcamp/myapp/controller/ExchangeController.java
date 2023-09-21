d001b685ed4c74c53a4da466c8156b9993fe00a4
data04190 <data04190@naver.com>
2023-09-21 18:18:17 +0900

    feat : 환전 요청 게시판 detail 페이지 추가

--- app-server/src/main/java/bitcamp/myapp/controller/ExchangeController.java
+++ app-server/src/main/java/bitcamp/myapp/controller/ExchangeController.java
@@ -2,15 +2,13 @@ package bitcamp.myapp.controller;
 
 import bitcamp.myapp.service.ExchangeService;
 import bitcamp.myapp.service.NcpObjectStorageService;
+import bitcamp.myapp.service.UserService;
 import bitcamp.myapp.vo.Exchange;
 import bitcamp.myapp.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.Date;
@@ -25,6 +23,9 @@ public class ExchangeController {
 
   @Autowired
   ExchangeService exchangeService;
+
+  @Autowired
+  UserService userService;  // UserService 주입
 
   @Autowired
   NcpObjectStorageService ncpObjectStorageService;
@@ -48,6 +49,23 @@ public class ExchangeController {
     return "redirect:/exchange/list";
   }
 
+  @GetMapping("delete")
+  public String delete(int no, HttpSession session) throws Exception {
+    User loginUser = (User) session.getAttribute("loginUser");
+    if (loginUser == null) {
+      return "redirect:/auth/form";
+    }
+
+    Exchange e = exchangeService.get(no);
+
+    if (e == null || e.getUser().getNo() != loginUser.getNo()) {
+      throw new Exception("해당 번호의 게시글이 없거나 삭제 권한이 없습니다.");
+    } else {
+      exchangeService.delete(e.getNo());
+      return "redirect:/exchange/list";
+    }
+  }
+
   @GetMapping("list")
   public void list(Model model) throws Exception {
     model.addAttribute("list", exchangeService.list());
@@ -57,23 +75,59 @@ public class ExchangeController {
   public String detail(@PathVariable int no, Model model) throws Exception {
     Exchange exchange = exchangeService.get(no);
     if (exchange != null) {
+      String content = exchange.getContent();
+      String[] parts = content.split(",");
+
+      // 각각의 부분을 모델에 추가
+      if (parts.length >= 3) {
+        model.addAttribute("name", parts[0]);
+        model.addAttribute("number", parts[1]);
+        model.addAttribute("bank", parts[2]);
+      }
+
       model.addAttribute("exchange", exchange);
     }
     return "exchange/detail";
   }
 
+  @PostMapping("update")
+  public String update(Exchange exchange,
+                       @RequestParam String name,
+                       @RequestParam String number,
+                       @RequestParam String bank,
+                       @RequestParam int userNo,
+                       HttpSession session) throws Exception {
 
-//  @GetMapping("detail/{no}")
-//  public String detail(@PathVariable int no, Model model) throws Exception {
-//    Exchange exchange = exchangeService.get(no);
-//    if (exchange != null) {
-//      exchangeService.increaseViewCount(no);
-//      model.addAttribute("exchane", exchange);
-//    }
-//    return "board/detail";
-//  }
+    User loginUser = (User) session.getAttribute("loginUser");
+    if (loginUser == null) {
+      return "redirect:/auth/form";
+    }
 
+    System.out.println("User No from request: " + userNo);
 
+    // userNo 값을 사용하여 User 객체를 검색합니다.
+    User user = userService.get(exchange.getUser().getNo());
+    if (user == null) {
+      throw new Exception("유효하지 않은 사용자 번호입니다.");
+    }
+    exchange.setUser(user);
 
+    Exchange existingExchange = exchangeService.get(exchange.getNo());
+    if (existingExchange == null || existingExchange.getUser().getNo() != loginUser.getNo()) {
+      throw new Exception("게시글이 존재하지 않거나 변경 권한이 없습니다.");
+    }
+
+    // 로그 추가
+    System.out.println("Requested Exchange No: " + exchange.getNo());
+    System.out.println("Logged User No: " + loginUser.getNo());
+    System.out.println("Exchange User No: " + existingExchange.getUser().getNo());
+
+    // 받아온 파라미터를 `,`로 합친다.
+    String combinedContent = String.join(",", name, number, bank);
+    exchange.setContent(combinedContent);
+
+    exchangeService.update(exchange);
+
+    return "redirect:/exchange/list";
+  }
 }
-
--- app-server/src/main/java/bitcamp/myapp/service/ExchangeService.java
+++ app-server/src/main/java/bitcamp/myapp/service/ExchangeService.java
@@ -8,4 +8,6 @@ public interface ExchangeService {
   int add(Exchange exchange) throws Exception;
   List<Exchange> list() throws Exception;
   Exchange get(int exchangeNo) throws Exception;
+  int update(Exchange exchange) throws Exception;
+  int delete(int exchangeNo) throws Exception;
 }
