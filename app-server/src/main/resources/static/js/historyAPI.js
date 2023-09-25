f2b287473b40419ca273020341f0386a95f70725
data04190 <data04190@naver.com>
2023-09-25 14:11:16 +0900

    feat : 입찰 내역 추가 - 환전신청 버튼 수정 필요

--- app-server/src/main/resources/static/js/historyAPI.js
+++ app-server/src/main/resources/static/js/historyAPI.js
@@ -1,29 +1,3 @@
-//document.getElementById('loadBidList').addEventListener('click', function() {
-//    document.getElementById('bidTable').style.display = 'table';
-//    fetch('/history/api/bidList')
-//    .then(response => response.json())
-//    .then(data => {
-//        const tbody = document.getElementById('bidListBody');
-//        tbody.innerHTML = ""; // 내용 초기화
-//        data.forEach((history, index) => {
-//            const tr = document.createElement('tr');
-//            tr.innerHTML = `
-//                <td>${index + 1}</td>
-//                <td>${history.article.title}</td>
-//                <td>${history.price}</td>
-//                <td>${history.winBid}</td>
-//                <td>${new Date(history.bidDate).toISOString().replace('T', ' ').substring(0, 19)}</td>
-//            `;
-//            tbody.appendChild(tr);
-//        });
-//    })
-//    .catch(error => {
-//        console.error('Error:', error);
-//        alert('입찰 내역을 불러오는 중 오류가 발생했습니다.');
-//    });
-//});
-
-
 // 함수로 만들어진 클릭 이벤트 핸들러
 function loadBidList() {
     document.getElementById('bidTable').style.display = 'table';
